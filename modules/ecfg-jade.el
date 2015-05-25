1d846615141b89b20ebe70dad0dbefa448669f95
Alexey Maslennikov <alexey@maslennikov.me>
2015-05-25 13:08:35 +0200

    fix for jade-mode recipe

--- modules/ecfg-jade.el
+++ modules/ecfg-jade.el
@@ -7,11 +7,16 @@
    (autoload 'jade-mode "jade-mode" nil t)
    (add-to-list 'auto-mode-alist '("\\.styl$" . stylus-mode))
    (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
-   (add-hook 'jade-mode-hook 'ecfg--jade-hook))
+   (add-hook 'jade-mode-hook 'ecfg--jade-hook)
+   (add-hook 'stylus-mode-hook 'ecfg--stylus-hook))
   )
 ;;;###autoload (ecfg-auto-module "\\.styl$" jade)
 ;;;###autoload (ecfg-auto-module "\\.jade$" jade)
 
 (defun ecfg--jade-hook ()
-  (setq tab-width 4)
+  ;; (setq tab-width 2)
   (setq comment-start "//-"))
+
+(defun ecfg--stylus-hook ()
+  ;; (setq tab-width 2)
+  (setq comment-start "//"))
