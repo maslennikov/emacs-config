2f4b3eb6ca9496aa73f63f53cbc2b162ec4399bc
Alexey Maslennikov <alexey@maslennikov.me>
2015-09-25 21:06:32 +0200

    FIX yaml-mode .yml extension support

--- modules/ecfg-yaml.el
+++ modules/ecfg-yaml.el
@@ -4,5 +4,5 @@
 (defun ecfg-yaml-module-init ()
   (ecfg-install yaml-mode
     (autoload 'yaml-mode "yaml-mode" nil t)
-    (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))))
-;;;###autoload (ecfg-auto-module "\\.yaml\\'" yaml)
+    (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))))
+;;;###autoload (ecfg-auto-module "\\.ya?ml\\'" yaml)
