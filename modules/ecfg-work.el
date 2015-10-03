1af29acb10f6575a4469ce33d076a17758f7d110
Alexey Maslennikov <alexey@maslennikov.me>
2015-10-03 16:38:31 +0200

    expanded the grep blacklist

--- modules/ecfg-work.el
+++ modules/ecfg-work.el
@@ -14,10 +14,13 @@
        (add-to-list 'grep-find-ignored-directories "build*")
        (add-to-list 'grep-find-ignored-directories "contrib")
        (add-to-list 'grep-find-ignored-directories "bin")
+       (add-to-list 'grep-find-ignored-directories "*cache")
 
        (add-to-list 'grep-find-ignored-directories "node_modules")
        (add-to-list 'grep-find-ignored-directories "bower_components")
-       (add-to-list 'grep-find-ignored-directories "uploads")))
+       (add-to-list 'grep-find-ignored-directories "uploads")
+
+       (add-to-list 'grep-find-ignored-files "*.min.js")))
 
   (eval-after-load "projectile"
     '(progn
