cabcefa3f318fc287c0a986a7ec2aff66a1ebf73
Alexey Maslennikov <alexey@maslennikov.me>
2015-10-05 09:23:13 +0200

    Setting up emacs to work with org-mode clocks

--- modules/ecfg-org.el
+++ modules/ecfg-org.el
@@ -10,6 +10,8 @@
    (global-set-key "\C-cl" 'org-store-link)
    (global-set-key "\C-ca" 'org-agenda)
    (global-set-key "\C-cc" 'org-capture)
+   (global-set-key (kbd "<f10>") 'org-clock-in-last)
+   (global-set-key (kbd "<f11>") 'org-clock-out)
 
    (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
    (add-to-list 'auto-mode-alist '("\\.org.txt\\'" . org-mode))
@@ -17,7 +19,10 @@
    (eval-after-load "org"
      '(progn
         ;; releasing the autocomplete shortcut
-        (define-key org-mode-map (kbd "<C-tab>") nil)))
+        (define-key org-mode-map (kbd "<C-tab>") nil)
+        (setq org-clock-persist 'history)
+        (org-clock-persistence-insinuate)
+        (setq org-clock-idle-time 10)))
 
    (add-hook 'org-mode-hook 'ecfg--org-hook)))
 
