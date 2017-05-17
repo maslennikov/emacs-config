40863e17aadc11bccad870f014bd1be551ac4fab
Alexey Maslennikov <alexey@maslennikov.me>
2017-05-17 12:55:36 +0200

    supporting python with yas and company

--- modules/ecfg-python.el
+++ modules/ecfg-python.el
@@ -8,4 +8,14 @@
 (defun ecfg--py-hook ()
   ;;specifying underscore as a member of symbol class instead of word class
   ;; (modify-syntax-entry ?_ ".")
+  (eval-after-load "company"
+    '(progn
+       (set
+        (make-local-variable 'company-backends)
+        '((company-dabbrev-code
+           company-keywords
+           company-dabbrev
+           company-yasnippet)))))
+
+  (yas-minor-mode)
   )
