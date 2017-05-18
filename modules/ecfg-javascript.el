d567a81d1782178f62da198102e68a21e873ddf3
Alexey Maslennikov <alexey@maslennikov.me>
2017-05-18 13:52:26 +0200

    js module supports interpreter-mode autoload

--- modules/ecfg-javascript.el
+++ modules/ecfg-javascript.el
@@ -4,34 +4,35 @@
 (defun ecfg-javascript-module-init ()
   (ecfg-install js2-mode
     (ecfg-with-local-autoloads
-   ;; (autoload 'js2-mode "js2-mode" nil t)
-   ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-   (add-to-list 'auto-mode-alist '("\\.jsx?$" . js2-jsx-mode))
+     ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+     (add-to-list 'auto-mode-alist '("\\.jsx?$" . js2-jsx-mode))
+     (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 
-   (eval-after-load 'js2-mode
-     '(progn
-        (setq
-         ;; TODO a lot of new goodies appeared in js2-mode,
-         ;; research customize-group
-         js2-basic-offset 2
-         js2-cleanup-whitespace t
-         js2-auto-indent-p t
-         js2-bounce-indent-p nil
-         js2-idle-timer-delay 0.5
-         js2-mirror-mode nil
-         js2-missing-semi-one-line-override t
-         js2-allow-keywords-as-property-names nil
-         js2-strict-missing-semi-warning nil
-         js2-pretty-multiline-declarations t)
+     (eval-after-load 'js2-mode
+       '(progn
+          (setq
+           ;; TODO a lot of new goodies appeared in js2-mode,
+           ;; research customize-group
+           js2-basic-offset 2
+           js2-cleanup-whitespace t
+           js2-auto-indent-p t
+           js2-bounce-indent-p nil
+           js2-idle-timer-delay 0.5
+           js2-mirror-mode nil
+           js2-missing-semi-one-line-override t
+           js2-allow-keywords-as-property-names nil
+           js2-strict-missing-semi-warning nil
+           js2-pretty-multiline-declarations t)
 
-        (define-key js2-mode-map (kbd "RET") 'js2-line-break)))
+          (define-key js2-mode-map (kbd "RET") 'js2-line-break)))
 
-   (add-hook 'js2-mode-hook 'ecfg--js-hook)
-   (add-hook 'js2-jsx-mode-hook #'(lambda ()
-     (setq-local sgml-basic-offset js2-basic-offset)))
+     (add-hook 'js2-mode-hook 'ecfg--js-hook)
+     (add-hook 'js2-jsx-mode-hook #'(lambda ()
+      (setq-local sgml-basic-offset js2-basic-offset)))
    )))
 
 ;;;###autoload (ecfg-auto-module "\\.jsx?$" javascript)
+;;;###autoload (ecfg-auto-module "node" javascript interpreter-mode-alist)
 
 (defun ecfg--manually-bounce-indent ()
   (interactive)
