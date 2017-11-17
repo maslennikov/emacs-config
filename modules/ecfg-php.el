13b17c833d524808974ce8fc837651b739794bb8
Alexey Maslennikov <alexey@maslennikov.me>
2017-11-17 19:24:54 +0300

    Fix custom php style init after php-mode load

--- modules/ecfg-php.el
+++ modules/ecfg-php.el
@@ -6,36 +6,40 @@
   (ecfg-install php-mode
     (autoload 'php-mode "php-mode" "Editing PHP" t)
     (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+    (eval-after-load 'php-mode
+      '(progn
+         (c-add-style
+          "maslennikov-php"
+          '("php"
+            (c-basic-offset . 4)
+            (tab-width . 4)
+            (c-indent-comments-syntactically-p t)
+            (fill-column . 80)
+            (c-offsets-alist . (
+              (arglist-intro . php-lineup-arglist-intro)
+              (arglist-cont . 0)
+              (arglist-cont-nonempty . +)
+              (arglist-close . php-lineup-arglist-close)
+              (case-label . 0)
+              (class-open . 0)
+              (comment-intro . 0)
+              (inlambda . 0)
+              (inline-open . 0)
+              (namespace-open . 0)
+              (lambda-intro-cont . +)
+              (label . +)
+              (statement-cont . php-lineup-hanging-semicolon)
+              (substatement-open . 0)
+              (topmost-intro-cont . (first php-lineup-cascaded-calls +))))
+            ))
+         ))
+
     (add-hook 'php-mode-hook 'ecfg--php-hook)
     )
 )
 ;;;###autoload (ecfg-auto-module "\\.php$" php)
 
 (defun ecfg--php-hook ()
-  (c-add-style
-  "maslennikov-php"
-  '("php"
-    (c-basic-offset . 4)
-    (tab-width . 4)
-    (c-indent-comments-syntactically-p t)
-    (fill-column . 80)
-    (c-offsets-alist . (
-      (arglist-close . php-lineup-arglist-close)
-      (arglist-cont . (first php-lineup-cascaded-calls 0))
-      (arglist-cont-nonempty . (first php-lineup-cascaded-calls c-lineup-arglist))
-      (arglist-intro . php-lineup-arglist-intro)
-      (case-label . 0)
-      (class-open . 0)
-      (comment-intro . 0)
-      (inlambda . 0)
-      (inline-open . 0)
-      (namespace-open . 0)
-      (lambda-intro-cont . +)
-      (label . +)
-      (statement-cont . php-lineup-hanging-semicolon)
-      (substatement-open . 0)
-      (topmost-intro-cont . (first php-lineup-cascaded-calls +))))
-    ))
-
   (c-set-style "maslennikov-php")
   (add-hook 'post-self-insert-hook 'ecfg-two-dots nil t))
